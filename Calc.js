f195c577a105082a79f92e326a580a1a3bb15116
Junior1923 <Junior.musseb@gmail.com>
2023-02-02 21:00:37 -0400

    act

--- Calc.js
+++ Calc.js
@@ -1,29 +1,31 @@
-var suma;
-var rest;
-var divi;
-var multi;
+
+
 class Calculadora {
     sumar(num1, num2) {
      console.log(num1,num2);
-     localStorage.setItem('suma:', suma = num1 + num2);
+     var suma = num1 + num2;
+     localStorage.setItem, ('suma:',  suma);
         return suma;
     }
 
     restar(num1, num2) {
         console.log(num1,num2);
-        localStorage.setItem('resta:', rest = num1 - num2);
+        var rest = num1 - num2;
+        localStorage.setItem('resta:', rest);
         return rest;
     }
 
     dividir(num1, num2) {
         console.log(num1,num2);
-        localStorage.setItem('division:',divi = num1 / num2)
+        var divi = num1 / num2;
+        localStorage.setItem('division:',divi )
         return divi;
     }
 
     multiplicar(num1, num2) {
       console.log(num1,num2);
-      localStorage.setItem('Multiplicacion:', multi = num1 * num2)
+      var multi = num1 * num2;
+      localStorage.setItem('Multiplicacion:', multi )
     
         return multi;
     }
